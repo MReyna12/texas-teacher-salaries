769f5f0fbdfb7d3ce6cea15636a4ecb3bd68f22f
Michael <MReyna12@live.com>
2022-06-07 15:40:38 -0500

    add try/catch statements to service file

--- src/services/teacher-salary-service.js
+++ src/services/teacher-salary-service.js
@@ -1,13 +1,21 @@
 const Salaries = require("../database/teacher-salaries");
 
 const getAllSalaries = () => {
-  const allSalaries = Salaries.getAllSalaries();
-  return allSalaries;
+  try {
+    const allSalaries = Salaries.getAllSalaries();
+    return allSalaries;
+  } catch (error) {
+    throw error;
+  }
 };
 
 const getSalariesByCityName = (cityName) => {
-  const salaryByCity = Salaries.getSalaryByCityName(cityName);
-  return salaryByCity;
+  try {
+    const salaryByCity = Salaries.getSalaryByCityName(cityName);
+    return salaryByCity;
+  } catch (error) {
+    throw error;
+  }
 };
 
 module.exports = {
