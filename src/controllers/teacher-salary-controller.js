7543f25e3bbb7d927a8d8642b8baac5d49e2140b
Michael <MReyna12@live.com>
2022-06-07 15:38:45 -0500

    add try catch statements to the controller file

--- src/controllers/teacher-salary-controller.js
+++ src/controllers/teacher-salary-controller.js
@@ -1,20 +1,43 @@
+const { getSalaryByCityName } = require("../database/teacher-salaries");
 const teacherSalaryService = require("../services/teacher-salary-service");
 
 const getAllSalaries = (req, res) => {
-  const allSalaries = teacherSalaryService.getAllSalaries();
-  res.send({
-    status: "OK",
-    data: allSalaries,
-  });
+  try {
+    const allSalaries = teacherSalaryService.getAllSalaries();
+    res.send({
+      status: "OK",
+      data: allSalaries,
+    });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 const getSalariesByCityName = (req, res) => {
   const cityName = req.params.name.toLowerCase();
-  const salariesByCity = teacherSalaryService.getSalariesByCityName(cityName);
-  res.send({
-    status: "OK",
-    data: salariesByCity,
-  });
+
+  if (!getSalaryByCityName(cityName)) {
+    res.status(400).send({
+      status: "FAILED",
+      data: {
+        error:
+          "Parameter 'cityName' can not be empty and must match an available city name",
+      },
+    });
+  }
+  try {
+    const salariesByCity = teacherSalaryService.getSalariesByCityName(cityName);
+    res.send({
+      status: "OK",
+      data: salariesByCity,
+    });
+  } catch (error) {
+    res
+      .status(error?.status || 500)
+      .send({ status: "FAILED", data: { error: error?.message || error } });
+  }
 };
 
 module.exports = {
