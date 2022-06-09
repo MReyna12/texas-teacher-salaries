6236fa5b49b67e2332a1eab000bb71e8de6632ff
Michael <MReyna12@live.com>
2022-06-09 17:14:34 -0500

    further server js revisions

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const teacherSalaryRouter = require("./routes/teacher-salary-routes");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use("/api/teacher-salaries", teacherSalaryRouter);
+app.use("/", teacherSalaryRouter);
 
 app.listen(PORT, () => {
   console.log(`The server is now running on port: ${PORT}.`);
