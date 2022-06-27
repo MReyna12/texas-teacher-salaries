2162a58c6547ddc2613f299f7e7eff85d2b60f05
Michael <MReyna12@live.com>
2022-06-27 10:59:30 -0500

    create simple fetch function to obtain data from db.json

--- main.js
+++ main.js
@@ -13,3 +13,20 @@ navToggle.addEventListener("click", () => {
     navToggle.setAttribute("aria-expanded", false);
   }
 });
+
+// Fetch school district data data from database
+function fetchSchoolDistrictData() {
+  const schoolDistrictData = "./src/database/db.json";
+  fetch(schoolDistrictData)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
+}
+
+fetchSchoolDistrictData();
+
+// Create table
