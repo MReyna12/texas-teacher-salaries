347aa5080e10783916ad24de4026e22d0117ebe9
Michael <MReyna12@live.com>
2022-06-29 10:39:13 -0500

    create separate file for salary determinations

--- src/helpers/create-table.js
+++ src/helpers/create-table.js
@@ -1,8 +1,9 @@
 import tableStyling from "./style-table.js";
 import tableHeaders from "./table-headers.js";
 import yoe from "./years-of-experience.js";
+import districtSalaries from "./salary.js";
 
-// Creates the table, with the columns determined by years of experience + number of salary differences per degree type
+// Creates the table, with the columns determined by years of experience + salary by degree type
 const createTable = {
   newTable: (schoolDistrictData) => {
     const jsonData = schoolDistrictData;
@@ -30,7 +31,7 @@ const createTable = {
       tableStyling.styleTable();
       tableHeaders.addTableHeaders();
       yoe.addYearsOfExperience(jsonData);
-      addSalaries(jsonData);
+      districtSalaries.addSalaries(jsonData);
       // If 2022-2023 salary data DOES NOT exist, then add a p tag that contains a message stating as much to the user
     } else {
       const noTableText = jsonData.austin_metro[1].school_year[1]["2022-2023"];
