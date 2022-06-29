d6d6a81ca8898f1cd30f533eab43cadebd1d1679
Michael <MReyna12@live.com>
2022-06-29 11:29:49 -0500

    cors updates

--- src/server.js
+++ src/server.js
@@ -5,6 +5,16 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use("/", teacherSalaryRouter);
 
 app.listen(PORT, () => {
