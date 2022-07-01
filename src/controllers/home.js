e64b4124e6341054af80064a3a4b4c73788ae15f
Michael <MReyna12@live.com>
2022-07-01 11:22:29 -0500

    create austin.ejs and allow navigation between index and austin within the nav bar

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,7 +1,18 @@
 //const path = require("path");
 
 module.exports = {
-  getIndex: (req, res) => {
-    res.render("index.ejs");
+  getIndex: async (req, res) => {
+    try {
+      await res.render("index.ejs");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  getAustin: async (req, res) => {
+    try {
+      await res.render("austin.ejs");
+    } catch (error) {
+      console.log(error);
+    }
   },
 };
--- src/routes/main-router.js
+++ src/routes/main-router.js
@@ -3,5 +3,6 @@ const homeController = require("../controllers/home");
 const router = express.Router();
 
 router.get("/", homeController.getIndex);
+router.get("/austin", homeController.getAustin);
 
 module.exports = router;
