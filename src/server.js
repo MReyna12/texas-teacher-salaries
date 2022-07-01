8360401f761513ce57e1ee9cd61aea546fe14912
Michael <MReyna12@live.com>
2022-07-01 11:09:13 -0500

    implement route/controller for the index.ejs page

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const teacherSalaryRouter = require("./routes/teacher-salary-routes");
+const mainRoutes = require("./routes/main-router");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+// Setup ejs and serve public files (css/images)
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Implement requisite cors information to allow for external use of api/ability to fetch using the heroku url
+app.use(cors());
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
@@ -16,7 +21,12 @@ app.use(function (req, res, next) {
   );
   next();
 });
+
+// API route
 app.use("/api", teacherSalaryRouter);
+
+// Front-end routes
+app.use("/", mainRoutes);
 
 app.listen(PORT, () => {
   console.log(`The server is now running on port: ${PORT}.`);
