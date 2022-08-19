6d75fa2dc2cc276c78fb2b39b69383bae64b13c3
Michael <MReyna12@live.com>
2022-08-19 14:36:28 -0500

    adjust image

--- teacher-salaries/teacher-salaries/pages/metro/[area]/index.js
+++ teacher-salaries/teacher-salaries/pages/metro/[area]/index.js
@@ -22,7 +22,6 @@ export async function getStaticProps(context) {
     .collection("district-information")
     .findOne({ metro: context.params.area }); // Based on what route is generated (via the getStaticPaths function below), search for the district data for the applicable metro area clicked by the user
   const allDistrictData = JSON.parse(JSON.stringify(districts));
-  console.log(allDistrictData);
 
   return {
     props: { allDistrictData },
@@ -36,10 +35,10 @@ export async function getStaticPaths() {
   const districts = await db
     .collection("district-information")
     .find()
+    .sort()
     .toArray(); // Get all of the collections and put them into an array
 
   const metroNames = districts.map((district) => district.metro); // Create an array of the metro area names
-  //console.log(metroNames);
 
   const paths = metroNames.map((metro) => ({ params: { area: metro } })); // Dynamically generate the routes for the metro pages
 
