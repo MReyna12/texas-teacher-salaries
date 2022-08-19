1cf291dab6ce64cbbf61520d4cbc0cb8cba809af
Michael <MReyna12@live.com>
2022-08-18 20:16:30 -0500

    names of districts now dynamically generated

--- teacher-salaries/teacher-salaries/components/Cards.js
+++ teacher-salaries/teacher-salaries/components/Cards.js
@@ -1,17 +1,40 @@
 import { nanoid } from "nanoid";
+import cardStyles from "../styles/Cards.module.css";
 
 const Cards = (props) => {
-  //console.log(props.districtData);
-
   const districts = props.districtData;
 
-  const cities = districts.map((district) => {
-    return <p key={nanoid()}>{district.city}</p>;
+  const individualDistrict = districts.map((district) => {
+    return (
+      <div className="layout-card-grid">
+        <div>
+          <a href="/metro/austin_metro/district/austin independent school district">
+            <div className="layout-district-card">
+              <div className="layout-flex layout-justify-content-center layout-align-items-center layout-district-box">
+                <div className="module-district-content module-text-align-center">
+                  <img
+                    className="module-district-logos"
+                    src="/images/austin-metro/austin-isd.png"
+                    alt="Austin ISD seal"
+                  />
+                  <div>
+                    <h4 key={nanoid()}>{district.abbreviated_name}</h4>
+                  </div>
+                  <div>
+                    <button key={nanoid()} className="module-austin-shadow-box">
+                      Learn more
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
+    );
   });
 
-  //createDistrictCard();
-
-  return <div>{cities}</div>;
+  return <div>{individualDistrict}</div>;
 };
 
 export default Cards;
