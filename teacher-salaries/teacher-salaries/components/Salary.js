c54132389a76068ccd2b79be208e00d7ceaa33cb
Michael <MReyna12@live.com>
2022-08-22 15:42:13 -0500

    create logic to render the salary table

--- teacher-salaries/teacher-salaries/components/Salary.js
+++ teacher-salaries/teacher-salaries/components/Salary.js
@@ -1,54 +1,110 @@
+import salaryStyles from "../styles/Salary.module.css";
+
 const Salary = (props) => {
+  // Data that is passed via the prop is in an array, so we use the 0 index to target the applicable data
   const districtData = props.data.singleDistrictData[0];
-  // Creates the table with the columns determined by years of experience + salary by degree type
-  // If 2022-2023 salary data exists, then add the table and rows/cells to match the data for the applicable school district
 
+  // If this value is falsy, then the function generateTableData is not going to run and the paragraph in the return statement will be displayed
   const yearsOfExperience = districtData.school_year[1]["2022-2023"].yoe;
-  //console.log(yearsOfExperience[0]);
 
-  const numberOfYears = Object.keys(yearsOfExperience);
-  //console.log(numberOfYears);
+  // If yearsOfExperience is truthy, then this function will run and generate all the data for the salary table
+  const generateTableData = () => {
+    const numberOfYears = Object.keys(yearsOfExperience);
 
-  const salaryByDegree = yearsOfExperience.map((salaryObject) => {
-    const bachelorSalary = salaryObject["10 months bachelor"];
-    return bachelorSalary;
-  });
+    const degreeType = Object.keys(yearsOfExperience[0]["0"]);
 
-  console.log(salaryByDegree);
+    const salaryByDegree = (degreeType) => {
+      return yearsOfExperience.map((salaryObject) => {
+        const salarykey = Object.keys(salaryObject)[0];
+        const salary = salaryObject[salarykey][degreeType];
 
-  const degreeType = Object.keys(yearsOfExperience[0]);
-  const salaryTableColumns = degreeType.map((degree) => {
-    let tableHeader;
-    if (degree.includes("bachelor")) {
-      tableHeader = "Bachelor";
-    } else if (degree.includes("master")) {
-      tableHeader = "Master";
-    } else if (degree.includes("doctorate")) {
-      tableHeader = "Doctorate";
-    }
+        return (
+          <td className={`${salaryStyles["module-table-spacing-border"]}`}>
+            {salary}
+          </td>
+        );
+      });
+    };
+
+    const salaryTableColumns = degreeType.map((degree) => {
+      let tableHeader;
+      if (degree.includes("bachelor")) {
+        tableHeader = "Bachelor";
+      } else if (degree.includes("master")) {
+        tableHeader = "Master";
+      } else if (degree.includes("doctorate")) {
+        tableHeader = "Doctorate";
+      }
+
+      return (
+        <tr className="layout-flex layout-flex-direction-column module-text-align-center">
+          {tableHeader === "Bachelor" && (
+            <>
+              <th
+                className={`${salaryStyles["module-table-spacing-border"]} state-text-bold`}
+              >
+                {tableHeader}
+              </th>
+              {salaryByDegree(["10 months bachelor"])}
+            </>
+          )}
+          {tableHeader === "Master" && (
+            <>
+              <th
+                className={`${salaryStyles["module-table-spacing-border"]} state-text-bold`}
+              >
+                {tableHeader}
+              </th>
+              {salaryByDegree(["10 months master"])}
+            </>
+          )}
+          {tableHeader === "Doctorate" && (
+            <>
+              <th
+                className={`${salaryStyles["module-table-spacing-border"]} state-text-bold`}
+              >
+                {tableHeader}
+              </th>
+              {salaryByDegree(["10 months doctorate"])}
+            </>
+          )}
+        </tr>
+      );
+    });
+
+    const yearsRow = numberOfYears.map((year) => {
+      return (
+        <td className={`${salaryStyles["module-table-spacing-border"]}`}>
+          {year}
+        </td>
+      );
+    });
 
     return (
-      <tr>
-        <th>{tableHeader}</th>
-      </tr>
+      <table>
+        <tbody>
+          <tr className="layout-flex layout-flex-direction-column module-text-align-center">
+            <th
+              className={`${salaryStyles["module-table-spacing-border"]} state-text-bold`}
+            >
+              Years
+            </th>
+            {yearsRow}
+          </tr>
+          {salaryTableColumns}
+        </tbody>
+      </table>
     );
-  });
-
-  const yearsRow = numberOfYears.map((year) => {
-    //console.log(yearsOfExperience[year][year]);
-    return <td>{year}</td>;
-  });
+  };
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Years</th>
-          {yearsRow}
-        </tr>
-        {salaryTableColumns}
-      </tbody>
-    </table>
+    <section>
+      {yearsOfExperience ? (
+        generateTableData()
+      ) : (
+        <p>{districtData.school_year[1]["2022-2023"]}</p>
+      )}
+    </section>
   );
 };
 
